60eaab13edb7d9376ab23c42be631001088355f7
Jonah <jonah.ahvonen@gmail.com>
2018-09-20 16:48:58 +0300

    CreateComponent

--- src/canvas-components.js
+++ src/canvas-components.js
@@ -2,18 +2,55 @@ function ComponentCanvas(canvasselector)
 {
 	if(CanvasComponentsUtil.IsUndefinedNullOrEmpty(canvasselector))
 	{
-		console.error('ComponentCanvas: Could not get canvas ' + canvasselector);
+		console.error('ComponentCanvas: Invalid selector ' + canvasselector);
 		return;
 	}
 
-	this.canvasid = canvasid;
-	this.canvas = document.querySelector(canvasselector);
+	this.canvasselector = canvasselector;
+
+	if(document.querySelector(this.canvasselector) === null)
+	{
+		console.error('ComponentCanvas: Could not get canvas ' + this.canvasselector);
+		return;
+	}
+
+	this.canvas = document.querySelector(this.canvasselector);
 	this.ctx = this.canvas.getContext('2d');
+
+	this.CanvasComponentCollection = new CanvasComponentCollection();
 }
 
 ComponentCanvas.prototype.CreateComponent = function(component)
 {
+	switch(component)
+	{
+		case CanvasComponent.BUTTON:
 
+			break;
+
+		case CanvasComponent.CHECKBOX:
+
+			break;
+
+		case CanvasComponent.PANEL:
+
+			break;
+
+		case CanvasComponent.RADIO:
+
+			break;
+
+		case CanvasComponent.TEXT_INPUT:
+
+			break;
+
+		case CanvasComponent.LABEL:
+
+			break;
+
+		default:
+			break;
+	}
 }
 
 ComponentCanvas.prototype.AddComponent = function()
